d322838af19a9f492b5d6d622b4181574edaafc7
akhi9550 <akhilc248@gmail.com>
2024-12-09 14:30:42 +0530

    WebSocket Simple code sender and reciver in different tab in browser

--- go.mod
+++ go.mod
@@ -3,19 +3,18 @@ module chat-websocket
 go 1.21.2
 
 require (
-	github.com/gofiber/fiber/v2 v2.52.5
-	github.com/gofiber/websocket/v2 v2.2.1
+	github.com/gofiber/fiber v1.14.6
+	github.com/gofiber/websocket v0.5.1
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/fasthttp/websocket v1.5.3 // indirect
-	github.com/google/uuid v1.5.0 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
--- main.go
+++ main.go
@@ -1,33 +1,167 @@
+// package main
+
+// import (
+// 	"log"
+
+// 	"github.com/gofiber/fiber/v2"
+// 	"github.com/gofiber/websocket/v2"
+// )
+
+// func main() {
+// 	app := fiber.New()
+
+// 	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+// 		defer c.Close()
+// 		for {
+// 			messageType, msg, err := c.ReadMessage()
+// 			if err != nil {
+// 				log.Println("Error reading message:", err)
+// 				break
+// 			}
+
+// 			log.Printf("Received: %s", msg)
+// 			if err := c.WriteMessage(messageType, msg); err != nil {
+// 				log.Println("Error writing message:", err)
+// 				break
+// 			}
+// 		}
+// 	}))
+
+// 	log.Fatal(app.Listen(":3000"))
+// }
+
+//---------->ws://localhost:3000/ws
+
+// package main
+
+// import (
+// 	"log"
+
+// 	"github.com/gofiber/fiber/v2"
+// 	"github.com/gofiber/websocket/v2"
+// )
+
+// var clients = make(map[*websocket.Conn]bool)
+
+// func main() {
+// 	app := fiber.New()
+// 	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+// 		defer func() {
+// 			delete(clients, c)
+// 			c.Close()
+// 		}()
+// 		clients[c] = true
+// 		log.Println("New client connected")
+
+// 		for {
+// 			messageType, msg, err := c.ReadMessage()
+// 			if err != nil {
+// 				log.Println("Error reading message:", err)
+// 				break
+// 			}
+
+// 			log.Printf("Received: %s", msg)
+
+// 			for client := range clients {
+// 				if client != c {
+// 					if err := client.WriteMessage(messageType, msg); err != nil {
+// 						log.Println("Error broadcasting message:", err)
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}))
+
+// 	log.Fatal(app.Listen(":3000"))
+// }
+
+// //----------->ws://localhost:3000/ws
+// //----------->ws://localhost:3000/ws
+
 package main
 
 import (
 	"log"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/websocket/v2"
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/websocket"
 )
+
+type client struct{}
+
+var clients = make(map[*websocket.Conn]client)
+var register = make(chan *websocket.Conn)
+var broadcast = make(chan string)
+var unregister = make(chan *websocket.Conn)
+
+func runHub() {
+	for {
+		select {
+		case connection := <-register:
+			clients[connection] = client{}
+			log.Println("connection registered")
+
+		case message := <-broadcast:
+			log.Println("message received:", message)
+
+			for connection := range clients {
+				if err := connection.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+					log.Println("write error:", err)
+
+					unregister <- connection
+					connection.WriteMessage(websocket.CloseMessage, []byte{})
+					connection.Close()
+				}
+			}
+
+		case connection := <-unregister:
+			delete(clients, connection)
+
+			log.Println("connection unregistered")
+		}
+	}
+}
 
 func main() {
 	app := fiber.New()
 
+	app.Static("/", "./home.html")
+
+	app.Use(func(c *fiber.Ctx) {
+		if websocket.IsWebSocketUpgrade(c) {
+			c.Next()
+		}
+	})
+
+	go runHub()
+
 	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
-		defer c.Close()
+		defer func() {
+			unregister <- c
+			c.Close()
+		}()
+
+		register <- c
+
 		for {
-			messageType, msg, err := c.ReadMessage()
+			messageType, message, err := c.ReadMessage()
 			if err != nil {
-				log.Println("Error reading message:", err)
-				break
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					log.Println("read error:", err)
+				}
+
+				return
 			}
 
-			log.Printf("Received: %s", msg)
-			if err := c.WriteMessage(messageType, msg); err != nil {
-				log.Println("Error writing message:", err)
-				break
+			if messageType == websocket.TextMessage {
+				broadcast <- string(message)
+			} else {
+				log.Println("websocket message received of type", messageType)
 			}
 		}
 	}))
 
-	log.Fatal(app.Listen(":3000"))
+	app.Listen(":3000")
 }
 
-//---------->ws://localhost:3000/ws
+// in browse -- localhost:3000/ws
